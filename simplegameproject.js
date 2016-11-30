d4151fb1e55a8d07123e5e98db24ff2b86266d5c
Jenna Lou <jennalouronquillo@gmail.com>
2016-11-30 08:10:35 -0800

    overlap  done

--- simplegameproject.js
+++ simplegameproject.js
@@ -1,26 +1,43 @@
-var xpos = 250
-document.addEventListener("keydown", function(e) {
-if (e.keyCode == 37) {
-  document.getElementById("bird").setAttribute("x", xpos - 15)
- xpos = xpos - 15;
+////////////////////////////bird's movement///////////////////
+document.addEventListener("keydown", function(e){
+ var birdSpotX = Number(document.getElementById("bird").getAttribute("x"));
+ var birdSpotY = Number(document.getElementById("bird").getAttribute("y"));
+ var foodX = Number(document.getElementById("foodfirst").getAttribute("x"));
+ var foodY = Number(document.getElementById("foodfirst").getAttribute("y"));
+ var width = 65;
+ var height = 65;
+
+ if (e.keyCode == 37) {
+   document.getElementById("bird").setAttribute("x", birdSpotX - 15)
+  birdSpotX = birdSpotX - 15;
+ }
+
+ else if (e.keyCode == 39) {
+  document.getElementById("bird").setAttribute("x", birdSpotX + 15)
+  birdSpotX = birdSpotX + 15;
+ }
+ if (e.keyCode == 38) {
+   document.getElementById("bird").setAttribute("y", birdSpotY - 15)
+  birdSpotY = birdSpotY - 15;
+ }
+
+ else if (e.keyCode == 40) {
+  document.getElementById("bird").setAttribute("y", birdSpotY + 15)
+  birdSpotY = birdSpotY + 15;
+ }
+
+if (birdSpotX > foodX && birdSpotX < foodX + 65 && birdSpotY > foodY && birdSpotY < foodY + 65){
+console.log("overlap")
 }
 
-else if (e.keyCode == 39) {
- document.getElementById("bird").setAttribute("x", xpos + 15)
- xpos = xpos + 15;
-}
-})
+//second ovverlap
 
+var foodX = Number(document.getElementById("foodsecond").getAttribute("x"));
+var foodY = Number(document.getElementById("foodsecond").getAttribute("y"));
 
-var ypos = 200
-document.addEventListener("keydown", function(e) {
-if (e.keyCode == 38) {
-  document.getElementById("bird").setAttribute("y", ypos - 15)
- ypos = ypos - 15;
+ if (birdSpotX > foodX && birdSpotX < foodX + 65 && birdSpotY > foodY && birdSpotY < foodY + 65){
+ console.log("overlap")
 }
+ })
 
-else if (e.keyCode == 40) {
- document.getElementById("bird").setAttribute("y", ypos + 15)
- ypos = ypos + 15;
-}
-})
+/////////////////////////////////////////overlap/////////////////////////////////////////
